de80aeea0eae63f0c6bf5e964e40cf281538e7bc
MickaelHm <harmeau.mickael@gmail.com>
2018-12-18 13:38:37 +0100

    Exercice complet

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tableau_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shopping = new List<string> { "Carottes", "Oignons", "Pommes de terre", "Salade", "Tomate" };
+            foreach (string product in shopping)
+            {
+                Console.WriteLine(product);
+            }
+
+
+        }
+    }
+}
